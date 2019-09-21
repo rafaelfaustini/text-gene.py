0ebb5473c19d3f72513469e55f51e0fd69d419b4
rafaelfaustini <contato@rafaelfaustini.com.br>
2019-09-21 13:51:37 -0300

    Eficiência melhorada
    Melhorado os logs e a eficiência do algoritmo

--- textgene.py
+++ textgene.py
@@ -28,7 +28,7 @@ class Populacao:
    tamanho = 0
    geracao=0
    fim = False
-   taxa = 0.1
+   taxa = 0.01
 
    def randomString(self, n):
       letters = string.printable
@@ -65,7 +65,7 @@ class Populacao:
       
       
 
-   def procriar(self,pai,mae):
+   def crossover(self,pai,mae):
       
       tamanho = len(pai)
       try:
@@ -74,7 +74,7 @@ class Populacao:
          else:
             marcador = (tamanho/2)+1
          marcador = int(marcador)
-         gene = self.mutacao(pai[:marcador]) + self.mutacao(mae[marcador:])
+         gene = self.mutacao(pai[:marcador] + mae[marcador:])
          filho = Elemento(gene)
          filho.calcularFitness(self.objetivo)
          return filho
@@ -83,7 +83,7 @@ class Populacao:
          
          
 
-   def selecionar(self):
+   def reproduzir(self):
       tamanho = len(self.lista)
       pivo = round((tamanho-1)*0.2)
       
@@ -95,10 +95,10 @@ class Populacao:
       for n in selecionados:
          try:
             if i%2 == 1: 
-               novo.append(self.procriar(n.gene,selecionados[aux].gene))
-               novo.append(self.procriar(selecionados[aux].gene, n.gene))
+               novo.append(self.crossover(n.gene,selecionados[aux].gene))
+               novo.append(self.crossover(selecionados[aux].gene, n.gene))
                if pivo > 0:
-                  novo.append(self.procriar(selecionados[aux].gene,n.gene))
+                  novo.append(self.crossover(selecionados[aux].gene,n.gene))
                   pivo -= 1
                aux+=1
             i+=1
@@ -116,26 +116,30 @@ class Populacao:
       return texto
 
    def ciclo(self):
-      while self.fim == False:
-         try: 
-            self.ordenar()
-            self.selecionar()
-            self.geracao += 1
-            if self.lista[0].fitness == 100:
-               self.parar()
-         except Exception as error:
-            print('Caught this error: ' + repr(error))
+      with open("log-runtime.txt", "a+") as arquivo: 
+         while self.fim == False:
+            try: 
+               self.ordenar()
+               self.reproduzir()
+               self.geracao += 1
+               arquivo.write("\nGeracao: %d Maior Fitness: %2f | Gene: %s \n"%
+                          (self.geracao, self.lista[0].fitness, self.lista[0].gene))
+               if self.lista[0].fitness == 100:
+                  self.parar()
+            except Exception as error:
+               print('Caught this error: ' + repr(error))
+         arquivo.close()
       try:
-         f=open("log.txt", "a+")
-         agora = datetime.now()
-         f.write("\n\nTempo de Execução: "+str(time.time() - self.inicio)+" segundos")
-         f.write("\n"+agora.strftime('%d/%m/%Y %H:%M'))
-         f.write("\nObjetivo: "+self.objetivo+"\n")
-         f.write("Geracoes: "+str(self.geracao)+"\n")
-         f.write("Resultado: "+self.lista[0].gene+"\n")
-         f.write("\nPopulacao Final: \n\n"+self.printLista(self.lista)+"\n\n")
-         f.close()
-         print("Relatorio Gerado com sucesso")
+         with open("log.txt", "a+") as f: 
+            agora = datetime.now()
+            f.write("\n\nTempo de Execução: "+str(time.time() - self.inicio)+" segundos")
+            f.write("\n"+agora.strftime('%d/%m/%Y %H:%M'))
+            f.write("\nObjetivo: "+self.objetivo+"\n")
+            f.write("Geracoes: "+str(self.geracao)+"\n")
+            f.write("Resultado: "+self.lista[0].gene+"\n")
+            f.write("\nPopulacao Final: \n\n"+self.printLista(self.lista)+"\n\n")
+            f.close()
+            print("Relatorio Gerado com sucesso")
       except Exception as error:
          print("Houve um erro ao gerar o relatório")
          print('Caught this error: ' + repr(error))    
