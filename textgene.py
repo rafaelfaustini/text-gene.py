ff02af6a439fe037f920aae4ec74f767f7bdfef9
rafaelfaustini <contato@rafaelfaustini.com.br>
2019-09-26 19:28:01 -0300

    Update textgene.py

--- textgene.py
+++ textgene.py
@@ -31,8 +31,10 @@ class Populacao:
    taxa = 0.01
 
    def randomString(self, n):
-      letters = string.printable
-      return ''.join(random.choice(letters) for i in range(n))          
+      letters = string.printable.replace('\t\n\r\x0b', '')
+      
+      r= random.SystemRandom()
+      return ''.join(r.choice(letters) for i in range(n))          
 
    def __init__(self,objetivo, tamanho):
       self.objetivo = objetivo
@@ -52,7 +54,8 @@ class Populacao:
       i=0
       novo = ""
       for n in elemento:
-         aux = random.random()
+         r = random.SystemRandom()
+         aux = r.random()
          try:
             if(aux < self.taxa):
                novo+= self.randomString(1)
@@ -67,14 +70,15 @@ class Populacao:
 
    def crossover(self,pai,mae):
       
-      tamanho = len(pai)
       try:
-         if tamanho%2 == 0:
-            marcador = tamanho/2
-         else:
-            marcador = (tamanho/2)+1
-         marcador = int(marcador)
-         gene = self.mutacao(pai[:marcador] + mae[marcador:])
+         gene = ""
+         aleatorio = random.SystemRandom()
+         for i in range(0, len(pai)):
+            r = aleatorio.random()
+            if r < 0.5:
+               gene+= pai[i]
+            else:
+               gene+= mae[i]
          filho = Elemento(gene)
          filho.calcularFitness(self.objetivo)
          return filho
@@ -85,7 +89,7 @@ class Populacao:
 
    def reproduzir(self):
       tamanho = len(self.lista)
-      pivo = round((tamanho-1)*0.2)
+      pivo = round((tamanho-1)*0.4)
       
       selecionados= self.lista[:(tamanho-pivo)]
       i=0
@@ -109,40 +113,54 @@ class Populacao:
    def parar(self):
       self.fim = True
 
-   def printLista(self,lista):
-      texto = ""
-      for i in lista:
-         texto+= i.gene+","
-      return texto
+   def avaliar(self):
+      if self.lista[0].fitness == 100:
+         self.parar()
+      else:
+         self.geracao += 1
 
-   def ciclo(self):
-      with open("log-runtime.txt", "a+") as arquivo: 
-         while self.fim == False:
-            try: 
-               self.ordenar()
-               self.reproduzir()
-               self.geracao += 1
-               arquivo.write("\nGeracao: %d Maior Fitness: %2f | Gene: %s \n"%
-                          (self.geracao, self.lista[0].fitness, self.lista[0].gene))
-               if self.lista[0].fitness == 100:
-                  self.parar()
-            except Exception as error:
-               print('Caught this error: ' + repr(error))
-         arquivo.close()
-      try:
-         with open("log.txt", "a+") as f: 
+   def gerarLog(self,runtime=None):
+      if runtime == None:
+         with open("log-"+datetime.now().strftime("%Y%m%d-%H%M%S"+".txt"), "a+") as f: 
             agora = datetime.now()
-            f.write("\n\nTempo de Execução: "+str(time.time() - self.inicio)+" segundos")
+            f.write("Tempo de Execução: "+str(time.time() - self.inicio)+" segundos")
             f.write("\n"+agora.strftime('%d/%m/%Y %H:%M'))
             f.write("\nObjetivo: "+self.objetivo+"\n")
             f.write("Geracoes: "+str(self.geracao)+"\n")
             f.write("Resultado: "+self.lista[0].gene+"\n")
-            f.write("\nPopulacao Final: \n\n"+self.printLista(self.lista)+"\n\n")
-            f.close()
+            f.write("\nPopulacao Final: \n"+self.printLista(self.lista)+"\n")
             print("Relatorio Gerado com sucesso")
+         f.close()
+      else:
+         runtime.write("\nGeracao: %d Maior Fitness: %2f | Gene: %s \n"%(self.geracao, self.lista[0].fitness, self.lista[0].gene))
+                       
+   def printLista(self,lista):
+      texto = ""
+      j=0
+      tamanho = len(lista)
+      for i in lista:
+         texto+= i.gene
+         if not j==tamanho-1:
+            texto+= ','
+         j+=1
+      return texto.replace("\n", "")
+
+   def ciclo(self):
+      try: 
+         with open("log-runtime.txt", "w") as arquivo: 
+            while self.fim == False:
+               
+               self.ordenar()
+               self.reproduzir()
+               self.gerarLog(runtime=arquivo)
+               self.avaliar()
+                       
+         self.gerarLog()
       except Exception as error:
-         print("Houve um erro ao gerar o relatório")
-         print('Caught this error: ' + repr(error))    
+         print('Houve um erro ' + repr(error))
+      finally:
+         arquivo.close
+                       
    def iniciar(self):
       self.fim=False
       self.ciclo()
@@ -152,12 +170,18 @@ class Populacao:
 
 
 try:
-   tamanho = int(sys.argv[2])
-   frase = sys.argv[1]
+   if sys.argv[1] == "-f" and len(sys.argv) == 4:
+      with open(sys.argv[2], 'r') as file:
+          frase = file.read().replace('\n', '')
+          tamanho = int(sys.argv[3])
+      file.close()
+   else:
+      tamanho = int(sys.argv[2])
+      frase = sys.argv[1]
+   p = Populacao(frase, tamanho)
+   p.iniciar()
 except:
    print("———————————————————————————————————————————————")
    print("\tArgumentos inválidos")
    print("———————————————————————————————————————————————")
    sys.exit("Argumentos inválidos")
-p = Populacao(frase, tamanho)
-p.iniciar()
